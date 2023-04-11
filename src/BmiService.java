97c2e93b2f8d1dec4a085b01ad8082f32178ce42
MaxFunk <funk-masik@yandex.ru>
2023-04-11 13:40:18 +0300

    inicial committ

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate( int mass, double height) {
-        double bmi = mass /(height * height);
+    public int calculate(int mass, double height) {
+        double bmi = mass / (height * height);
         return (int) bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height  = 1.75; //метров
+        double height = 1.75; //метров
         int mass = 75; //килограмм
-        int index = service.calculate(mass,height );
+        int index = service.calculate(mass, height);
         System.out.println(index + "bmi");
     }
 }
